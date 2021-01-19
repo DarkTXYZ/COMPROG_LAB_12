d28e349eea5a2987b155fd1a36d9ef675c92c9a3
DarkTXYZ <47694361+DarkTXYZ@users.noreply.github.com>
2021-01-19 20:01:41 +0700

    Update lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,25 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[] , int n , double b[]){
+    double sum = 0 , sumstd = 0 , prod = 1 , sumHar = 0 , Max = -9999 , Min = 9999;
+    for(int i = 0 ; i < n ; ++i){
+        sum += a[i];
+        prod *= a[i];
+        sumHar += 1.0 / a[i];
+        if(a[i] > Max)
+            Max = a[i];
+        if(a[i] < Min)
+            Min = a[i];
+    }
+    b[0] = sum / n;
+    for(int i = 0 ; i < n ; ++i){
+        sumstd += (a[i] - b[0])*(a[i] - b[0]);
+    }
+    b[1] = sqrt(sumstd / n);
+    b[2] = pow(prod , 1.0/n);
+    b[3] = double(n) / sumHar; 
+    b[4] = Max;
+    b[5] = Min;   
+}
